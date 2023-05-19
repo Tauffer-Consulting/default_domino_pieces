491d7bd30f9145ed82b2779b2ad6e777de38d9fc
luiz <luiz_tauffer@hotmail.com>
2023-05-19 17:14:37 +0200

    enum

--- pieces/SimpleLogPiece/models.py
+++ pieces/SimpleLogPiece/models.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel, Field
+from enum import Enum
 
+class InputEnum(str, Enum):
+    option1 = "option1"
+    option2 = "option2"
+    option3 = "option3"
 
 class InputModel(BaseModel):
     """
@@ -20,6 +25,10 @@ class InputModel(BaseModel):
     input_bool: bool = Field(
         default=False,
         description='Input boolean to be logged.'
+    )
+    input_enum: InputEnum = Field(
+        default=InputEnum.option1,
+        description='Input enum to be logged.'
     )
     input_list: list = Field(
         default=[],
