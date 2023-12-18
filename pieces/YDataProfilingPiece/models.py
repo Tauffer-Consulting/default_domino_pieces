218f6d75ced42432f46c592c0166163b17ac53d7
vinicvaz <vinicvaz95@gmail.com>
2023-12-18 10:12:18 -0300

    fix model

--- pieces/YDataProfilingPiece/models.py
+++ pieces/YDataProfilingPiece/models.py
@@ -3,7 +3,13 @@ from typing import Optional
 
 
 class InputModel(BaseModel):
-    data_path: str = Field(title='CSV file path', description='Path to the CSV file to be profiled')
+    data_path: str = Field(
+        title='CSV file path',
+        description='Path to the CSV file to be profiled',
+        json_schema_extra={
+            "from_upstream": "always"
+        }
+    )
     report_tile: Optional[str] = Field(title='Report title', description='Title of the report', default='Profiling Report')
 
 class OutputModel(BaseModel):
