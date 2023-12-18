5e8dc968a43e06d3471b9a26b73e07b2e13e89ff
vinicvaz <vinicvaz95@gmail.com>
2023-12-18 10:00:19 -0300

    ydataprofiling piece

---
+++ pieces/YDataProfilingPiece/models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class InputModel(BaseModel):
+    data_path: str = Field(title='CSV file path', description='Path to the CSV file to be profiled')
+    report_tile: Optional[str] = Field(title='Report title', description='Title of the report', default='Profiling Report')
+
+class OutputModel(BaseModel):
+    profile_file_path: str = Field(title='Output file path', description='Path to the output file.')
